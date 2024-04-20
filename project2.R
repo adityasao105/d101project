642ac5ca4bece0f50df739614914f2e13a085ef4
Aditya Sao <adityasao105@gmail.com>
2024-04-20 11:46:06 -0400

    confused with rpart

--- project2.R
+++ project2.R
@@ -34,18 +34,35 @@ training_csv$wifi <- factor(training_csv$wifi, levels = c(0,1),
 
 
 ggplot(training_csv, aes(x=price_range, fill = blue)) +
-  geom_bar(position = "dodge")
+  geom_bar(position = "dodge") #nothing
 ggplot(training_csv, aes(x=price_range, fill = dual_sim)) +
-  geom_bar(position = "dodge")
+  geom_bar(position = "dodge") #nothing
 ggplot(training_csv, aes(x=price_range, fill = four_g)) +
-  geom_bar(position = "dodge")
+  geom_bar(position = "dodge")#nothing
 ggplot(training_csv, aes(x=price_range, fill = three_g)) +
-  geom_bar(position = "dodge")
+  geom_bar(position = "dodge")#more 3g sold than not 3g in each section
 ggplot(training_csv, aes(x=price_range, fill = touch_screen)) +
-  geom_bar(position = "dodge")
+  geom_bar(position = "dodge")#nothing
 ggplot(training_csv, aes(x=price_range, fill = wifi)) +
-  geom_bar(position = "dodge")
+  geom_bar(position = "dodge")#nothing
 ggplot(training_csv, aes(x=ram, y=n_cores,
                                  shape=price_range, color=price_range)) +
-  geom_point() +
-  labs(title = "Cores,Ram, and Price", x="Ram", y="Cores")
+geom_point() +
+labs(title = "Cores,Ram, and Price", x="Ram", y="Cores") # the higher the ram the higer price range
+ggplot(training_csv,aes(x=price_range,y=battery_power))+geom_boxplot() #higher battery power higher price
+ggplot(training_csv,aes(x=price_range,y=px_width))+geom_boxplot() #higher width higher price
+ggplot(training_csv,aes(x=price_range,y=px_height))+geom_boxplot() #higher height higher price
+
+
+mse<-c() #how do i get mse for catagorical values?
+# for(CP in seq(from=0.00001, to=0.001,0.00001)){
+# tree=rpart(price_range~three_g+ram+battery_power+px_width+px_height,data=training_data,method="class",control=rpart.control(cp=CP))
+# pred<-predict(tree,training_data,type="class")
+# 
+# }
+
+
+
+
+
+
