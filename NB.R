9fec6c2cc08a9df392acc90bd66a1bd402eb034a
Aditya Sao <adityasao105@gmail.com>
2024-04-20 19:29:32 -0400

    used klaR cm instead of our own

--- NB.R
+++ NB.R
@@ -17,11 +17,10 @@ training_data$price_range <- factor(training_data$price_range, levels=c(0, 1, 2,
 testing_data$price_range <- factor(testing_data$price_range, levels=c(0, 1, 2, 3), labels=c("l", "m", "h", "vh"))
 
 nb_model<-NaiveBayes(price_range ~ ram + battery_power + px_height + px_width, data=training_data)
+nb_model
 pred<-predict(nb_model,testing_data)
 testing_data$prediction<-pred
 testing_data$prediction <- factor(testing_data$price_range, levels=c(0, 1, 2, 3), labels=c("l", "m", "h", "vh"))
 
-cm <- table(testing_data$prediction, testing_data$price_range)
-# cm
-accuracy <- sum(cm[1], cm[6], cm[11], cm[16]) / sum(cm[1:16])
-accuracy
+errormatrix(testing_data$price_range,testing_data$prediction)
+
