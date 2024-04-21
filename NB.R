7de1247917bd280b05f0c4be56c64483d2da4a2c
Summma <pinto.alex05@gmail.com>
2024-04-20 20:56:57 -0400

    Fixed NaiveBayes (you have to do pred$class)

--- NB.R
+++ NB.R
@@ -1,8 +1,4 @@
-library(rpart)
-library(rpart.plot)
 library(rsample)
-library(pheatmap)
-library(ggplot2)
 library(klaR)
 # setwd("/Users/adityasao/data101/project2")
 set.seed(1234)
@@ -13,14 +9,13 @@ split <- initial_split(training_csv, prop = 0.7)
 training_data <- training(split)
 testing_data<-testing(split)
 
-training_data$price_range <- factor(training_data$price_range, levels=c(0, 1, 2, 3), labels=c("l", "m", "h", "vh"))
-testing_data$price_range <- factor(testing_data$price_range, levels=c(0, 1, 2, 3), labels=c("l", "m", "h", "vh"))
+training_data$price_range <- factor(training_data$price_range, levels=c(0, 1, 2, 3), labels=c("Low", "Medium", "High", "Luxury"))
+testing_data$price_range <- factor(testing_data$price_range, levels=c(0, 1, 2, 3), labels=c("Low", "Medium", "High", "Luxury"))
 
 nb_model<-NaiveBayes(price_range ~ ram + battery_power + px_height + px_width, data=training_data)
-nb_model
 pred<-predict(nb_model,testing_data)
-testing_data$prediction<-pred
-testing_data$prediction <- factor(testing_data$price_range, levels=c(0, 1, 2, 3), labels=c("l", "m", "h", "vh"))
 
-errormatrix(testing_data$price_range,testing_data$prediction)
+cm <- table(pred$class, testing_data$price_range)
+accuracy <- sum(cm[1], cm[6], cm[11], cm[16]) / sum(cm[1:16])
+accuracy
 
